9e796ae0c0b0ddfeb25a06670b829eeacf2b6ee6
Ali25319 <Tofikali25319@gmail.com>
2021-12-21 20:24:13 +0530

    my fistcommit

--- src/App.js
+++ src/App.js
@@ -16,8 +16,17 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <div>
+      <h1> first name</h1>
+      <h2> last name</h2>
+      <h3>contact number</h3>
+      <h4>email address</h4>
+      <h5>sign up</h5>
+      <h6>login</h6>
+    </div>
         </a>
       </header>
+      
     </div>
   );
 }
